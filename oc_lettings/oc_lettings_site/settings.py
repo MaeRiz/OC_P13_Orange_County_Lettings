592e9e12891cac3b83e88eb97c5f4cc13035d606
Mahery Gonin <mahery.gonin@gmail.com>
2021-11-29 10:52:12 +0100

    Ajout de variables d'environnement

--- oc_lettings/oc_lettings_site/settings.py
+++ oc_lettings/oc_lettings_site/settings.py
@@ -1,4 +1,5 @@
 import os
+import env
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -8,7 +9,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'fp$9^593hsriajg$_%=5trot9g!1qa@ew(o-1#@=&4%=hp46(s'
+SECRET_KEY = env.SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
