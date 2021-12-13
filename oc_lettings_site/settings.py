b552ab8b8326af9e84286909c5ff5f89e5946429
Mahery Gonin <mahery.gonin@gmail.com>
2021-12-13 17:33:02 +0100

    Update settings for allowed all hosts

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = 'fp$9^593hsriajg$_%=5trot9g!1qa@ew(o-1#@=&4%=hp46(s'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
